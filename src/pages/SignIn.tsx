962e681b51adb4bcbbb17c267f1948a708e996bc
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-10 14:25:39 -0300

    add Button component and integrate it into SignIn form; update Input component to accept dynamic type

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,5 @@
 import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
@@ -11,6 +12,8 @@ export function SignIn() {
       />
 
       <Input required legend="Senha" type="password" placeholder="123456" />
+
+      <Button>Entrar</Button>
     </form>
   );
 }
