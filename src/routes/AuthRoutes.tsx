fbaa3ae1a403c508e5e78269519184f75694a91a
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-10 15:16:53 -0300

    add NotFound page and integrate it into AuthRoutes for handling 404 errors

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -3,6 +3,8 @@ import { SignIn } from "../pages/SignIn";
 import { AuthLayout } from "../components/AuthLayout";
 import { SignUp } from "../pages/SignUp";
 
+import { NotFound } from "../pages/NotFound";
+
 export function AuthRoutes() {
   return (
     <Routes>
@@ -10,6 +12,8 @@ export function AuthRoutes() {
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Route>
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
