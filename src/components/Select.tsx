5c3b74e86ff7df2123f224809d6346c57986a093
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-11 15:34:13 -0300

    add category selection to Refund form with dynamic options

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -14,7 +14,6 @@ export function Select({ legend, children, ...rest }: Props) {
       <select
         className="w-full h-12 rounded-lg border border-gray-300 px-4 text-sm text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300"
         {...rest}
-        value=""
       >
         <option value="" disabled hidden>
           Selecione
