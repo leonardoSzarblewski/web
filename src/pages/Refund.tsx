2b1907eb69263cd5e3645cead4a3cbf0a7cc10f9
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-11 15:39:45 -0300

    refactor layout of Refund form to group category selection and value input

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -18,18 +18,23 @@ export function Refund() {
       </header>
 
       <Input required legend="Nome da solicitação" />
-      <Select
-        required
-        legend="Categoria"
-        value={category}
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        {CATEGORIES_KEYS.map((category) => (
-          <option key={category} value={category}>
-            {CATEGORIES[category].name}
-          </option>
-        ))}
-      </Select>
+
+      <div className="flex gap-4">
+        <Select
+          required
+          legend="Categoria"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          {CATEGORIES_KEYS.map((category) => (
+            <option key={category} value={category}>
+              {CATEGORIES[category].name}
+            </option>
+          ))}
+        </Select>
+
+        <Input required legend="Valor" />
+      </div>
     </form>
   );
 }
