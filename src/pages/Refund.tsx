3f6ee37081418c96516ee3b4a815f6530cd68abb
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-11 16:09:20 -0300

    refactor Refund form to include state management and submit handling

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -4,11 +4,26 @@ import { Input } from "../components/Input";
 import { Select } from "../components/Select";
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Upload } from "../components/Upload";
+import { Button } from "../components/Button";
 
 export function Refund() {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [filename, setFilename] = useState<File | null>(null);
+
+  function onSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    console.log(name, amount, category, filename);
+  }
+
   return (
-    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-lg">
+    <form
+      onSubmit={onSubmit}
+      className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-lg"
+    >
       <header>
         <h1 className="text-xl font-bold text-gray-100">
           Solicitação de reembolso
@@ -18,7 +33,12 @@ export function Refund() {
         </p>
       </header>
 
-      <Input required legend="Nome da solicitação" />
+      <Input
+        required
+        legend="Nome da solicitação"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
 
       <div className="flex gap-4">
         <Select
@@ -34,10 +54,24 @@ export function Refund() {
           ))}
         </Select>
 
-        <Input required legend="Valor" />
+        <Input
+          required
+          legend="Valor"
+          value={amount}
+          onChange={(event) => setAmount(event.target.value)}
+        />
       </div>
 
-      <Upload filename="teste leo" />
+      <Upload
+        filename={filename && filename.name}
+        onChange={(event) =>
+          event.target.files && setFilename(event.target.files[0])
+        }
+      />
+
+      <Button type="submit" isLoading={isLoading}>
+        Enviar
+      </Button>
     </form>
   );
 }
