115675ef79c66d43c26fb25b13075439e5d285fe
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-13 16:18:47 -0300

    feat: add Loading component for improved user experience during loading states

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,21 @@
 import { BrowserRouter } from "react-router";
 
+import { Loading } from "../components/Loading";
+
 import { AuthRoutes } from "./AuthRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
-
 import { EmployeeRoutes } from "./EmployeeRoutes";
 
+const isLoading = false;
+
 export function Routes() {
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <BrowserRouter>
-      <ManagerRoutes />
+      <AuthRoutes />
     </BrowserRouter>
   );
 }
