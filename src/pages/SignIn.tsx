9b493e87701c5e80a24e91d73803fbc9fdfb9390
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-17 15:25:52 -0300

    feat: refactor SignIn component to use useActionState for form handling

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,34 +1,39 @@
-import { useState } from "react";
+import { useActionState } from "react";
+
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
 export function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [state, formAction, isLoading] = useActionState(signIn, {
+    email: "",
+    password: "",
+  });
 
-  function onSubmit(event: React.FormEvent) {
-    event.preventDefault();
+  async function signIn(prevenState: any, formData: FormData) {
+    const email = formData.get("email");
+    const password = formData.get("password");
 
-    console.log(email, password);
+    return { email, password };
   }
 
   return (
-    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+    <form action={formAction} className="w-full flex flex-col gap-4">
       <Input
+        name="email"
         required
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
-        onChange={(event) => setEmail(event.target.value)}
+        defaultValue={String(state?.email)}
       />
 
       <Input
+        name="password"
         required
         legend="Senha"
         type="password"
         placeholder="123456"
-        onChange={(event) => setPassword(event.target.value)}
+        defaultValue={String(state?.password)}
       />
 
       <Button type="submit" isLoading={isLoading}>
