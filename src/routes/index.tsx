93a678f2cb311dcf6fb4d728c7672f98e7e9c389
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-19 13:37:25 -0300

    feat: integrate save function in AuthContext and update SignIn component to utilize it

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,15 +10,8 @@ import { EmployeeRoutes } from "./EmployeeRoutes";
 
 const isLoading = false;
 
-const session = {
-  user: {
-    role: "",
-  },
-};
-
 export function Routes() {
-  const context = useAuth();
-  console.log(context);
+  const { session } = useAuth();
 
   function Route() {
     switch (session?.user.role) {
