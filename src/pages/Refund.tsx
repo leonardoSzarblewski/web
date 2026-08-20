d9922c7a580fe08f3eab09146dea8e454d14dc20
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-20 18:44:37 -0300

    feat: update Refund component to manage file upload and filename handling

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -27,7 +27,7 @@ export function Refund() {
   const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [filename, setFilename] = useState<File | null>(null);
+  const [file, setFile] = useState<File | null>(null);
 
   const navigate = useNavigate();
   const params = useParams<{ id: string }>();
@@ -42,6 +42,15 @@ export function Refund() {
     try {
       setIsLoading(true);
 
+      if (!file) {
+        return alert("Selecione um arquivo de comprovante ");
+      }
+
+      const fileUploadForm = new FormData();
+      fileUploadForm.append("file", file);
+
+      const response = await api.post("/uploads", fileUploadForm);
+
       const data = refundSchema.parse({
         name,
         category,
@@ -50,8 +59,7 @@ export function Refund() {
 
       await api.post("/refunds", {
         ...data,
-        filename:
-          "111111111111111111111111111111111111111111111111111111111.png",
+        filename: response.data.filename,
       });
 
       navigate("/confirm", { state: { fromSubmit: true } });
@@ -129,9 +137,9 @@ export function Refund() {
         </a>
       ) : (
         <Upload
-          filename={filename && filename.name}
+          filename={file && file.name}
           onChange={(event) =>
-            event.target.files && setFilename(event.target.files[0])
+            event.target.files && setFile(event.target.files[0])
           }
         />
       )}
