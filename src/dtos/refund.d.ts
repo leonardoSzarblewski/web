2a7421b02c9ffdf1324a1caf44bcad8461c040ea
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-21 15:59:19 -0300

    feat: update RefundItemProps to use description instead of category and adjust Dashboard to handle refund data mapping

--- src/dtos/refund.d.ts
+++ src/dtos/refund.d.ts
@@ -11,7 +11,7 @@ type RefundAPIResponse = {
 };
 
 type RefundsPaginationAPIResponse = {
-  refunds: RefundsPaginationAPIResponse[];
+  refunds: RefundAPIResponse[];
   pagination: {
     page: number;
     perPage: number;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -12,21 +12,13 @@ import { Button } from "../components/Button";
 import { RefundItem, type RefundItemProps } from "../components/RefundItem";
 import { Pagination } from "../components/Pagination";
 
-const REFUND_EXAMPLE = {
-  id: "123",
-  name: "Leonardo",
-  category: "Transporte",
-  amount: formatCurrency(34.5),
-  categoryImg: CATEGORIES["transport"].icon,
-};
-
 const PER_PAGE = 5;
 
 export function Dashboard() {
   const [name, setName] = useState("");
   const [page, setPage] = useState(1);
   const [totalOfPage, setTotalOfPage] = useState(0);
-  const [refunds, setRefunds] = useState<RefundItemProps[]>([REFUND_EXAMPLE]);
+  const [refunds, setRefunds] = useState<RefundItemProps[]>([]);
 
   async function fetchRefunds() {
     try {
@@ -34,7 +26,17 @@ export function Dashboard() {
         `/refunds?name=${name.trim()}&page=${page}&perPage=${PER_PAGE}`,
       );
 
-      console.log(response.data);
+      setRefunds(
+        response.data.refunds.map((refund) => ({
+          id: refund.id,
+          name: refund.user.name,
+          description: refund.name,
+          amount: formatCurrency(refund.amount),
+          categoryImg: CATEGORIES[refund.category].icon,
+        })),
+      );
+
+      setTotalOfPage(response.data.pagination.totalPages);
     } catch (error) {
       console.log(error);
 
